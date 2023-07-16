cec273a85efe0675cf76fe640fcd505e7ae6a19c
aidencjswo@gmail.com <aidencjswo@gmail.com>
2023-07-17 00:45:07 +0900

    채팅ㅅㅂ.

--- src/page/chat.js
+++ src/page/chat.js
@@ -3,37 +3,40 @@ import { useEffect, useRef, useState } from "react"
 function Chat(){
     let [serverChat,setServerChat] = useState()
     let [chatLog,setChatLog] = useState([])
-
+    const socketRef = useRef(null);
     const inputRef = useRef()
 
     useEffect(()=>{
-        socket.onopen = function(){
+        socketRef.current = new WebSocket("ws://localhost:8080/socket");
+
+        socketRef.current.onopen = function(){
             console.log("연결 성공")
         }
 
+        socketRef.current.onmessage = function(e) {
+            let copy = [...chatLog]
+            copy.push(e.data)
+            setChatLog(copy)
+            
+        }
+
         return()=>{
-            socket.close();
+            socketRef.current.close();
             console.log("연결 끊어짐")
         }
     },[])
 
-    var socket = new WebSocket("ws://localhost:8080/socket");
 
+ 
     const sendMessage = (e) =>{
-        socket.send(inputRef.current.value)
+        socketRef.current.send(inputRef.current.value)
         let copy = [...chatLog]
         copy.push(inputRef.current.value)
         setChatLog(copy)
         inputRef.current.value = ""; // input 요소의 값 비우기
     }
 
-    socket.onmessage = function(e) {
-        let copy = [...chatLog]
-        console.log(e.data)
-        copy.push(e.data)
-        setChatLog(copy)
-        
-    }
+
 
     const handleKeyDown = (e) => {
         if (e.key === "Enter") {
@@ -42,17 +45,23 @@ function Chat(){
     };
 
     return(
-        <>
+        <>  
             <div className = "chatRoom">
+                {
+                    console.log(chatLog)
+                }
+                {
+                    
+                    chatLog.map((a,i)=>(
+                        <div key={i}>{a}</div>
+                    ))
+                }
             <input 
                 id="textInput" 
                 ref={inputRef}
                 onKeyDown={handleKeyDown}
                 ></input>
-            <button onClick={()=>{
-                sendMessage()
-                
-            }}>보내기</button>
+            <button onClick={sendMessage}>보내기</button>
             </div>
         </>
     )
